1e343547ba1d68c8011a8e15ab390f317be054c3
GustavoLarocca <gustavo97ar@hotmail.com>
2022-07-04 20:09:54 -0300

    repair fail aa

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola"; 'bienvenidos';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = a;
+const nuevoNum = aa;
 
 // Crea una variable booleana:
 const nuevoBool = true;
